006fc1bb4b501f1080bcdeb093ea94b9a284e8b0
Ivan Veselov <veselov.iva@gmail.com>
2017-02-18 01:29:13 +0300

    Add javadoc to Lazy interface.

--- Lazy/src/main/java/ru/spbau/bachelor2015/veselov/hw01/Lazy.java
+++ Lazy/src/main/java/ru/spbau/bachelor2015/veselov/hw01/Lazy.java
@@ -2,6 +2,12 @@ package ru.spbau.bachelor2015.veselov.hw01;
 
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * An interface which represents an evaluation. It will be computed once and it's result will be memorized for
+ * subsequent queries.
+ *
+ * @param <T> type of evaluation result.
+ */
 public interface Lazy<T> {
     @Nullable T get();
 }
