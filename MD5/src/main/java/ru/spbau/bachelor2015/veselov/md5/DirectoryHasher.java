769b760723c9262fa008629272d976b5390696e9
Ivan Veselov <veselov.iva@gmail.com>
2017-03-30 14:52:19 +0300

    Initial commit with IDEA project and base classes skeletons.

---
+++ MD5/src/main/java/ru/spbau/bachelor2015/veselov/md5/DirectoryHasher.java
@@ -0,0 +1,15 @@
+package ru.spbau.bachelor2015.veselov.md5;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+
+public final class DirectoryHasher {
+    public static @NotNull MD5Hash getHash(final @NotNull Path path) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static @NotNull MD5Hash getHashConcurrent(final @NotNull Path path) {
+        throw new UnsupportedOperationException();
+    }
+}
