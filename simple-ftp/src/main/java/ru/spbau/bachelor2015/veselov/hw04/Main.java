323987ae440810607250f6d9ef0db514cebf7239
Ivan Veselov <veselov.iva@gmail.com>
2017-04-07 21:28:17 +0300

    Add Main class.

---
+++ simple-ftp/src/main/java/ru/spbau/bachelor2015/veselov/hw04/Main.java
@@ -0,0 +1,9 @@
+package ru.spbau.bachelor2015.veselov.hw04;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Server();
+    }
+}
