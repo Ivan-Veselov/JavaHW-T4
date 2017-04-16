fa7cccfe4e2ed143edc64bf5c2afaed87b36194e
Ivan Veselov <veselov.iva@gmail.com>
2017-04-16 20:44:44 +0300

    Add MessageTransmitter class.

---
+++ simple-ftp/src/main/java/ru/spbau/bachelor2015/veselov/hw04/MessageTransmitter.java
@@ -0,0 +1,50 @@
+package ru.spbau.bachelor2015.veselov.hw04;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.channels.WritableByteChannel;
+import java.util.LinkedList;
+
+/**
+ * Transmitter can write a queued sequence of messages to a specified channel.
+ */
+public class MessageTransmitter {
+    private final @NotNull WritableByteChannel channel;
+
+    private final @NotNull LinkedList<MessageWriter> writers = new LinkedList<>();
+
+    /**
+     * Creates a new transmitter.
+     *
+     * @param channel a channel to which new transmitter will be writing messages.
+     */
+    public MessageTransmitter(final @NotNull WritableByteChannel channel) {
+        this.channel = channel;
+    }
+
+    /**
+     * Adds a new message to the queue.
+     *
+     * @param data a message content.
+     */
+    public void addMessage(final @NotNull byte[] data) {
+        writers.add(new MessageWriter(channel, data));
+    }
+
+    /**
+     * Makes an attempt to write a message to a channel.
+     *
+     * @throws IOException if any IO exception occurs during writing.
+     */
+    public void write() throws IOException {
+        if (writers.isEmpty()) {
+            return;
+        }
+
+        MessageWriter writer = writers.getFirst();
+        if (writer.write()) {
+            writers.poll();
+        }
+    }
+}
--- simple-ftp/src/main/java/ru/spbau/bachelor2015/veselov/hw04/Server.java
+++ simple-ftp/src/main/java/ru/spbau/bachelor2015/veselov/hw04/Server.java
@@ -1,6 +1,7 @@
 package ru.spbau.bachelor2015.veselov.hw04;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.spbau.bachelor2015.veselov.hw04.exceptions.MessageNotReadException;
 import ru.spbau.bachelor2015.veselov.hw04.exceptions.MessageWithNegativeLengthException;
 
@@ -21,12 +22,21 @@ import java.util.Map;
 public class Server {
     private final static int port = 10000;
 
-    private final static @NotNull byte[] data = new byte[] {1, 2, 3};
+    private final static @NotNull byte[] data = new byte[] {'H', 'i', '!'};
+
+    private @Nullable Selector selector;
+
+    private @Nullable ServerSocketChannel serverSocketChannel;
 
     private final @NotNull Map<SelectionKey, MessageReader> messageReaders = new HashMap<>();
 
+    private final @NotNull Map<SelectionKey, MessageTransmitter> messageTransmitters = new HashMap<>();
+
     // TODO: handle exceptions
-    public Server() throws IOException {
+    public Server() {
+    }
+
+    public void start() throws IOException {
         try (Selector selector = Selector.open();
              ServerSocketChannel serverSocketChannel = ServerSocketChannel.open()) {
             serverSocketChannel.socket().bind(new InetSocketAddress(port));
@@ -39,45 +49,60 @@ public class Server {
                 for (SelectionKey key : selector.selectedKeys()) {
                     if (key.channel().equals(serverSocketChannel)) {
                         if (!key.isAcceptable()) {
-                            continue;
+                            acceptNewConnection();
                         }
 
-                        SocketChannel socketChannel = serverSocketChannel.accept();
-                        if (socketChannel == null) {
-                            continue;
-                        }
-
-                        socketChannel.configureBlocking(false);
-                        SelectionKey socketChannelKey = socketChannel.register(selector,
-                                                                SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-                        messageReaders.put(socketChannelKey, new MessageReader(socketChannel));
                         continue;
                     }
 
                     if (key.isReadable()) {
-                        MessageReader reader = messageReaders.get(key);
+                        readMessage(key);
+                    }
 
-                        try {
-                            if (!reader.read()) {
-                                continue;
-                            }
-                        } catch (MessageWithNegativeLengthException e) {
-                            // TODO: send an answer
-                        }
-
-                        try {
-                            reader.getMessage();
-                        } catch (MessageNotReadException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                        reader.reset();
-                        // TODO: send an answer
+                    if (key.isWritable()) {
+                        messageTransmitters.get(key).write();
                     }
                 }
 
                 selector.selectedKeys().clear();
             }
         }
+    }
+
+    private void acceptNewConnection() throws IOException {
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        if (socketChannel == null) {
+            return;
+        }
+
+        socketChannel.configureBlocking(false);
+
+        // TODO: only SelectionKey.OP_READ required
+        SelectionKey socketChannelKey = socketChannel.register(selector,
+                                                        SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        messageReaders.put(socketChannelKey, new MessageReader(socketChannel));
+        messageTransmitters.put(socketChannelKey, new MessageTransmitter(socketChannel));
+    }
+
+    private void readMessage(final @NotNull SelectionKey key) throws IOException {
+        MessageReader reader = messageReaders.get(key);
+
+        try {
+            if (!reader.read()) {
+                return;
+            }
+        } catch (MessageWithNegativeLengthException e) {
+            return;
+        }
+
+        try {
+            reader.getMessage();
+        } catch (MessageNotReadException e) {
+            throw new RuntimeException(e);
+        }
+
+        reader.reset();
+
+        messageTransmitters.get(key).addMessage(data);
     }
 }
