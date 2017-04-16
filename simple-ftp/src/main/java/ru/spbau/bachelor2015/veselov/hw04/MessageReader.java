8b2008b3b64957837a5392c7f3e30b734ecaa73a
Ivan Veselov <veselov.iva@gmail.com>
2017-04-16 18:14:09 +0300

    Add MessageReader class.

---
+++ simple-ftp/src/main/java/ru/spbau/bachelor2015/veselov/hw04/MessageReader.java
@@ -0,0 +1,78 @@
+package ru.spbau.bachelor2015.veselov.hw04;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.spbau.bachelor2015.veselov.hw04.exceptions.MessageNotReadException;
+import ru.spbau.bachelor2015.veselov.hw04.exceptions.MessageWithNegativeLengthException;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+
+/**
+ * A reader object must be created for reading messages of specific format from a ReadableByteChannel. Message format
+ * is: four bytes denoting the length of the message, a sequence of bytes which amount is equal to the length denoted by
+ * first four bytes.
+ */
+public class MessageReader {
+    private final @NotNull ReadableByteChannel channel;
+
+    private boolean isLengthRead = false;
+
+    private @Nullable ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+
+    private @Nullable ByteBuffer messageBuffer;
+
+    /**
+     * Creates a new MessageReader which will read from a specified channel.
+     *
+     * @param channel a channel to read from.
+     */
+    public MessageReader(final @NotNull ReadableByteChannel channel) {
+        this.channel = channel;
+    }
+
+    /**
+     * Makes an attempt to read a message from a channel.
+     *
+     * @return true if the message was fully read, false otherwise.
+     * @throws IOException if any IO exception occurs during reading.
+     * @throws MessageWithNegativeLengthException if the length of the message is negative.
+     */
+    public boolean read() throws IOException, MessageWithNegativeLengthException {
+        if (!isLengthRead) {
+            channel.read(lengthBuffer);
+            if (lengthBuffer.hasRemaining()) {
+                return false;
+            }
+
+            isLengthRead = true;
+
+            lengthBuffer.flip();
+            int length = lengthBuffer.getInt();
+            lengthBuffer = null;
+
+            if (length < 0) {
+                throw new MessageWithNegativeLengthException();
+            }
+
+            messageBuffer = ByteBuffer.allocate(length);
+        }
+
+        channel.read(messageBuffer);
+        return !messageBuffer.hasRemaining();
+    }
+
+    /**
+     * Returns the content of the read message.
+     *
+     * @throws MessageNotReadException if message hasn't been read.
+     */
+    public @NotNull byte[] getMessage() throws MessageNotReadException {
+        if (messageBuffer == null || messageBuffer.hasRemaining()) {
+            throw new MessageNotReadException();
+        }
+
+        return messageBuffer.array().clone();
+    }
+}
