62790ab3ffb2522a6f47e7ae8ca67fc9a588b038
Ivan Veselov <veselov.iva@gmail.com>
2017-04-27 05:08:12 +0300

    Redirect logging to file.

--- 04-SimpleFTP/server/src/main/java/ru/spbau/bachelor2015/veselov/hw04/server/Server.java
+++ 04-SimpleFTP/server/src/main/java/ru/spbau/bachelor2015/veselov/hw04/server/Server.java
@@ -29,7 +29,6 @@ import java.util.List;
 /**
  * Server class can accept a connections and deal with them by answering on requests.
  *
- * TODO: remove logging in client interface
  * TODO: look at gradle files
  * TODO: travis
  */
