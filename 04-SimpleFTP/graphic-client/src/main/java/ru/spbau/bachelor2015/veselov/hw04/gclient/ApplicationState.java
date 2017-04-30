9bdd978929b8d76cf7f54a7563929ac6ce4fb93a
Ivan Veselov <veselov.iva@gmail.com>
2017-04-30 12:28:10 +0300

    Add ability to navigate in server tracked folder through client.

--- 04-SimpleFTP/graphic-client/src/main/java/ru/spbau/bachelor2015/veselov/hw04/gclient/ApplicationState.java
+++ 04-SimpleFTP/graphic-client/src/main/java/ru/spbau/bachelor2015/veselov/hw04/gclient/ApplicationState.java
@@ -7,17 +7,22 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.spbau.bachelor2015.veselov.hw04.client.Client;
 import ru.spbau.bachelor2015.veselov.hw04.client.exceptions.ConnectionWasClosedException;
+import ru.spbau.bachelor2015.veselov.hw04.gclient.exceptions.ServerAddressIsNotSetException;
 import ru.spbau.bachelor2015.veselov.hw04.messages.util.FileEntry;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.UnresolvedAddressException;
 import java.nio.channels.UnsupportedAddressTypeException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 public final class ApplicationState {
     private @Nullable InetSocketAddress serverAddress;
+
+    private @Nullable Path currentFolder;
 
     private final @NotNull ObservableList<FileEntry> currentFolderObservable = FXCollections.observableArrayList(
         new FileEntry(Paths.get("root/folder1"), true),
@@ -34,35 +39,51 @@ public final class ApplicationState {
         return serverAddress;
     }
 
-    public void setServerAddress(final @Nullable InetSocketAddress serverAddress) {
+    public @Nullable Path getCurrentFolder() {
+        return currentFolder;
+    }
+
+    public void setServerAddress(final @NotNull InetSocketAddress serverAddress) {
+        loadFolderContent(serverAddress, Paths.get("")).ifPresent(entries -> {
+            this.serverAddress = serverAddress;
+
+            currentFolderObservable.clear();
+            currentFolderObservable.addAll(entries);
+        });
+    }
+
+    public void setCurrentFolder(final @NotNull Path path) throws ServerAddressIsNotSetException {
         if (serverAddress == null) {
-            this.serverAddress = null;
-            return;
+            throw new ServerAddressIsNotSetException();
         }
 
-        List<FileEntry> entries;
+        loadFolderContent(serverAddress, path).ifPresent(entries -> {
+            currentFolder = path;
+
+            currentFolderObservable.clear();
+            currentFolderObservable.addAll(entries);
+        });
+    }
+
+    public @NotNull ObservableList<FileEntry> getCurrentFolderObservable() {
+        return currentFolderObservable;
+    }
+
+    private @NotNull Optional<List<FileEntry>> loadFolderContent(final @NotNull InetSocketAddress serverAddress,
+                                                                 final @NotNull Path folder) {
         try {
-            entries = Client.list(serverAddress, Paths.get(""));
+            return Optional.of(Client.list(serverAddress, folder));
         } catch (IOException |
-                 UnresolvedAddressException |
-                 UnsupportedAddressTypeException |
-                 ConnectionWasClosedException e) {
+                UnresolvedAddressException |
+                UnsupportedAddressTypeException |
+                ConnectionWasClosedException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Error");
             alert.setHeaderText("A connection failure");
             alert.setContentText("Failed to download data from given server address.");
             alert.showAndWait();
 
-            return;
+            return Optional.empty();
         }
-
-        this.serverAddress = serverAddress;
-
-        currentFolderObservable.clear();
-        currentFolderObservable.addAll(entries);
-    }
-
-    public @NotNull ObservableList<FileEntry> getCurrentFolderObservable() {
-        return currentFolderObservable;
     }
 }
--- 04-SimpleFTP/graphic-client/src/main/java/ru/spbau/bachelor2015/veselov/hw04/gclient/ui/FileTableProducer.java
+++ 04-SimpleFTP/graphic-client/src/main/java/ru/spbau/bachelor2015/veselov/hw04/gclient/ui/FileTableProducer.java
@@ -2,9 +2,11 @@ package ru.spbau.bachelor2015.veselov.hw04.gclient.ui;
 
 import javafx.beans.binding.StringBinding;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import org.jetbrains.annotations.NotNull;
 import ru.spbau.bachelor2015.veselov.hw04.gclient.ApplicationState;
+import ru.spbau.bachelor2015.veselov.hw04.gclient.exceptions.ServerAddressIsNotSetException;
 import ru.spbau.bachelor2015.veselov.hw04.messages.util.FileEntry;
 
 public final class FileTableProducer {
@@ -33,6 +35,28 @@ public final class FileTableProducer {
         });
 
         table.getColumns().addAll(fileNameColumn, fileTypeColumn);
+
+        table.setRowFactory(param -> {
+            TableRow<FileEntry> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() != 2 || row.isEmpty()) {
+                    return;
+                }
+
+                FileEntry entry = row.getItem();
+                if (!entry.isDirectory()) {
+                    return;
+                }
+
+                try {
+                    state.setCurrentFolder(row.getItem().getPath());
+                } catch (ServerAddressIsNotSetException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            return row;
+        });
 
         table.setItems(state.getCurrentFolderObservable());
         return table;
