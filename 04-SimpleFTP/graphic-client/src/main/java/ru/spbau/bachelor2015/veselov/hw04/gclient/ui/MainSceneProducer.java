4f40063634ca4a06fdb52758275e6114ef4e844e
Ivan Veselov <veselov.iva@gmail.com>
2017-05-03 22:42:58 +0300

    Set vgrow for root vbox in main scene.

--- 04-SimpleFTP/graphic-client/src/main/java/ru/spbau/bachelor2015/veselov/hw04/gclient/ui/MainSceneProducer.java
+++ 04-SimpleFTP/graphic-client/src/main/java/ru/spbau/bachelor2015/veselov/hw04/gclient/ui/MainSceneProducer.java
@@ -1,16 +1,21 @@
 package ru.spbau.bachelor2015.veselov.hw04.gclient.ui;
 
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import org.jetbrains.annotations.NotNull;
 import ru.spbau.bachelor2015.veselov.hw04.gclient.ApplicationModel;
 
 public final class MainSceneProducer {
     public static @NotNull Scene produce(final @NotNull ApplicationModel model) {
+        TableView fileTable = FileTableProducer.produce(model);
+
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(ToolBarProducer.produce(model), FileTableProducer.produce(model));
+        vBox.getChildren().addAll(ToolBarProducer.produce(model), fileTable);
 
         vBox.setFillWidth(true);
+        VBox.setVgrow(fileTable, Priority.ALWAYS);
 
         Scene scene = new Scene(vBox);
 
