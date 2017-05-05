e8c933e2e8d959fcbc837bdf6fd467fb998a4c49
Ivan Veselov <veselov.iva@gmail.com>
2017-05-05 22:11:30 +0300

    Generify testing

--- 05-MyJunit/src/test/java/ru/spbau/bachelor2015/veselov/hw05/TesterTest.java
+++ 05-MyJunit/src/test/java/ru/spbau/bachelor2015/veselov/hw05/TesterTest.java
@@ -2,6 +2,7 @@ package ru.spbau.bachelor2015.veselov.hw05;
 
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import ru.spbau.bachelor2015.veselov.hw05.examples.*;
@@ -15,56 +16,50 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
 // TODO add tests with invalid testing classes
-// TODO generify testing
 public class TesterTest {
     @Test
     public void testOnePassingTestClass() throws Exception {
-        List<TestReport> report = new Tester(OnePassingTestClass.class).test();
-
-        assertThat(report, contains(passReport()));
+        testClass(OnePassingTestClass.class, passReport());
     }
 
     @Test
     public void testOneFailingTestClass() throws Exception {
-        List<TestReport> report = new Tester(OneFailingTestClass.class).test();
-
-        assertThat(report, contains(failureReport(Exception.class)));
+        testClass(OneFailingTestClass.class, failureReport(Exception.class));
     }
 
     @Test
     public void testClassWithSimpleTests() throws Exception {
-        List<TestReport> report = new Tester(ClassWithSimpleTests.class).test();
-
-        assertThat(report, containsInAnyOrder(passReport(),
-                                              passReport(),
-                                              failureReport(Exception.class),
-                                              failureReport(Exception.class)));
+        testClass(ClassWithSimpleTests.class,
+                                                passReport(),
+                                                passReport(),
+                                                failureReport(Exception.class),
+                                                failureReport(Exception.class));
     }
 
     @Test
     public void testFailingBeforeClass() throws Exception {
-        List<TestReport> report = new Tester(FailingBeforeClass.class).test();
-
-        assertThat(report, contains(failureReport(Exception.class)));
+        testClass(FailingBeforeClass.class, failureReport(Exception.class));
     }
 
     @Test
     public void testFailingAfterClass() throws Exception {
-        List<TestReport> report = new Tester(FailingAfterClass.class).test();
-
-        assertThat(report, contains(failureReport(Exception.class)));
+        testClass(FailingAfterClass.class, failureReport(Exception.class));
     }
 
     @Test
     public void testBeforeAfterCombinationClass() throws Exception {
-        List<TestReport> report = new Tester(BeforeAfterCombinationClass.class).test();
+        testClass(BeforeAfterCombinationClass.class,
+                                                        passReport(),
+                                                        failureReport(Exception1.class),
+                                                        failureReport(Exception2.class));
+    }
 
-        assertThat(report, containsInAnyOrder(passReport(),
-                                              failureReport(Exception1.class),
-                                              failureReport(Exception2.class)));
+    private void testClass(final @NotNull Class<?> clazz, final @NotNull Matcher<? super TestReport>... matchers)
+            throws Exception {
+        List<TestReport> report = new Tester(clazz).test();
+        assertThat(report, containsInAnyOrder(matchers));
     }
 
     private @NotNull PassReportMatcher passReport() {
