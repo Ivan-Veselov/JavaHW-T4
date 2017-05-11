eb163b1a21159069ea881bc29b1403eae0b410db
Ivan Veselov <veselov.iva@gmail.com>
2017-05-11 15:00:20 +0300

    Add IDEA project and basic game logic functionality.

---
+++ Pairs/src/main/java/ru/spbau/bachelor2015/veselov/pairs/Game.java
@@ -0,0 +1,89 @@
+package ru.spbau.bachelor2015.veselov.pairs;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static ru.spbau.bachelor2015.veselov.pairs.Game.ChoiceResult.FIRST_IN_PAIR;
+import static ru.spbau.bachelor2015.veselov.pairs.Game.ChoiceResult.MATCHED;
+import static ru.spbau.bachelor2015.veselov.pairs.Game.ChoiceResult.NOT_MATCHED;
+
+public class Game {
+    private final int fieldSize;
+
+    private final int[][] field;
+
+    private final boolean[][] isMatched;
+
+    private Index2 chosenCell = null;
+
+    public Game(final int fieldSize) {
+        if (fieldSize % 2 == 1) {
+            throw new IllegalArgumentException();
+        }
+
+        this.fieldSize = fieldSize;
+
+        field = new int[fieldSize][fieldSize];
+
+        final int numberOfPairs = fieldSize * fieldSize / 2;
+        int[] numbers = new int[2 * numberOfPairs];
+        int pointer = 0;
+        for (int i = 0; i < numberOfPairs; i++) {
+            numbers[pointer++] = i;
+            numbers[pointer++] = i;
+        }
+
+        Collections.shuffle(Arrays.asList(numbers));
+
+        pointer = 0;
+        for (int i = 0; i < fieldSize; i++) {
+            for (int j = 0; j < fieldSize; j++) {
+                field[i][j] = numbers[pointer++];
+            }
+        }
+
+        isMatched = new boolean[fieldSize][fieldSize];
+    }
+
+    public int getFieldSize() {
+        return fieldSize;
+    }
+
+    public @NotNull ChoiceResult choose(final @NotNull Index2 cell) {
+        if (!isValid(cell)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (isMatched[cell.getX()][cell.getY()]) {
+            throw new IllegalArgumentException();
+        }
+
+        if (chosenCell == null) {
+            chosenCell = cell;
+            return FIRST_IN_PAIR;
+        }
+
+        if (chosenCell.equals(cell)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (field[cell.getX()][cell.getY()] != field[chosenCell.getX()][chosenCell.getY()]) {
+            chosenCell = null;
+            return NOT_MATCHED;
+        }
+
+        isMatched[chosenCell.getX()][chosenCell.getY()] = true;
+        isMatched[cell.getX()][cell.getY()] = true;
+        chosenCell = null;
+
+        return MATCHED;
+    }
+
+    private boolean isValid(final @NotNull Index2 cell) {
+        return 0 <= cell.getX() && cell.getX() < fieldSize && 0 <= cell.getY() && cell.getY() < fieldSize;
+    }
+
+    public enum ChoiceResult { FIRST_IN_PAIR, MATCHED, NOT_MATCHED }
+}
---
+++ Pairs/src/main/java/ru/spbau/bachelor2015/veselov/pairs/Index2.java
@@ -0,0 +1,32 @@
+package ru.spbau.bachelor2015.veselov.pairs;
+
+import com.sun.istack.internal.Nullable;
+
+public class Index2 {
+    private final int x;
+
+    private final int y;
+
+    public Index2(final int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(final @Nullable Object object) {
+        if (!(object instanceof Index2)) {
+            return false;
+        }
+
+        Index2 other = (Index2) object;
+        return this.x == other.x && this.y == other.y;
+    }
+}
